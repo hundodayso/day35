6eeff809cbf79a3659c4c5c9d25e03f6b8868b8d
hundodayso <solder-elision.0y@icloud.com>
2025-01-22 22:02:00 +0000

    Started Request

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-test
+from KEY import api_key
+import requests
+lat = '45.077713'
+long = '6.702118'
+
+api_key = api_key
+
+payload = {'lat': lat, 'lon':long, 'appid':api_key, 'units': 'metric' }
+
+
+r1 = requests.get('https://api.openweathermap.org/data/2.5/forecast?', params=payload)
+print(r1.json())
+
+
+
+
+
+
