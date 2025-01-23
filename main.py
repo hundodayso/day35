14b3850b9802abee0bf8442c8061456e6fe433a9
hundodayso <solder-elision.0y@icloud.com>
2025-01-23 22:21:11 +0000

    Started Request

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 from KEY import api_key
 import requests
-lat = '45.077713'
-long = '6.702118'
+from twilio.rest import Client
+# lat = 45.077713
+# long = 6.702118
+lat = 45.326908
+long = 14.441000
 
 api_key = api_key
 
-payload = {'lat': lat, 'lon':long, 'appid':api_key, 'units': 'metric' }
+payload = {'lat': lat,
+           'lon':long,
+           'appid':api_key,
+           'units': 'metric',
+           'cnt': 4
+           }
 
 
-r1 = requests.get('https://api.openweathermap.org/data/2.5/forecast?', params=payload)
-print(r1.json())
+response = requests.get('https://api.openweathermap.org/data/2.5/forecast?', params=payload)
+response.raise_for_status()
+
+#print(response.json())
+weather_data = response.json()
+will_rain = False
+
+for time in weather_data['list']:
+    for item in time['weather']:
+        if int(item['id']) < 700:
+            will_rain = True
+            print(item["id"])
+if will_rain:
+    print("Bring an umbrella")
 
 
 
